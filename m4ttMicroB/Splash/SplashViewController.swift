f8128782ee7b353f324774015d944c4c74e219fb
Alex Petrovskyi <a.petrovskyi95@yahoo.com>
2022-11-23 19:30:04 +0200

    Version 1.0.0 Build 6:
    1) Added Splash animation for the storyBoard (need to fix something)

---
+++ m4ttMicroB/Splash/SplashViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SplashViewController.swift
+//  m4ttMicroB
+//
+//  Created by Mac on 23.11.2022.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView =  UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200 ))
+        imageView.image = UIImage(named: "tvmazeLogo")
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        imageView.center = view.center
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            self.animate()
+        })
+    }
+    
+    private func animate() {
+        UIView.animate(withDuration: 1, animations: {
+            let size = self.view.frame.size.width * 3
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - size
+            
+            self.imageView.frame = CGRect(x: -(diffX/2),
+                                          y: diffY/2,
+                                          width: size,
+                                          height: size
+            )
+        })
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageView.alpha = 0
+        }, completion: {done in
+            if done {
+                let mainVC = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+                let navController = UINavigationController(rootViewController: mainVC)
+
+                navController.modalPresentationStyle = .fullScreen
+                self.present(navController, animated:true, completion: nil)
+            }
+        })
+    }
+    
+    
+}
+    
+    
+  
+
